c74c2c65b239af1f6b7e364844d2a82a9a7fbd29
Mohan Krishnan <mohangk@gmail.com>
2012-01-28 16:12:23 +0800

    modified the sign up page to take in all necessary details

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :institution_id, :phone_number
   validates :institution_id, presence: true, uniqueness: true
   validates :name, presence: true
   belongs_to :institution
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,20 +2,34 @@ require 'spec_helper'
 
 describe "Users" do
   describe "sign up" do
-
-    it 'provides a form for users to sign up', do
+    before do
+      Factory(:institution)
+    end
+    it 'provides a form for users to sign up the forward them to the profile page', do
       visit profile_path
       page.should have_content 'You need to sign in'
       page.should have_link 'Sign up'
       click_link 'Sign up'
       page.should have_content 'Sign up'
       fill_in 'Email', with: 'test@test.com'
+      fill_in 'Name', with: 'Jojoman'
       fill_in 'Password', with: 'password'
       fill_in 'Password confirmation', with: 'password'
       select 'Multimedia University', from: 'Institution'
       fill_in 'Phone number', with: '012123123123'
       click_button 'Sign up'
+      page.should_not have_content 'error'
+      page.should have_content 'signed up successfully'
+      page.current_path.should == profile_path
     end
+  end
+
+  describe 'profile' do
+    before do
+      Factory(:user)
+    end
+
+    it 'allows a user that is logged in to be able to see his '
   end
 
 
