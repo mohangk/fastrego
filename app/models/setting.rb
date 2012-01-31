315751b87a1d3aea629a58427645b7b03709a501
Mohan Krishnan <mohangk@gmail.com>
2012-01-31 11:59:25 +0800

    Ensure setting handles casses where the setting does not exist in the DB by returning nil

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,9 +2,11 @@ class Setting < ActiveRecord::Base
   validates :key, presence: true, uniqueness: true
   def self.method_missing(*args)
     if args.length == 1
-      puts "me here"
-      puts args[0]
-      self.find_by_key(args[0]).value
+
+      setting = self.find_by_key(args[0])
+      if not setting.nil?
+        setting.value
+      end
     else
       super
     end
--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -7,7 +7,11 @@ describe Setting do
   end
   it { should validate_presence_of :key }
   it { should validate_uniqueness_of :key }
-  it "has the ability to expose the values in the key column as attributes of the Setting class" do
+  it 'has the ability to expose the values in the key column as methods of the Setting class' do
     Setting.enable_pre_registration.should == 'True'
+  end
+
+  it 'returns nil if the setting does not exist in the model' do
+    Setting.does_not_exist.should == nil
   end
 end
