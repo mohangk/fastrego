a2f5c75867cbfb56490a4737dbae57906d2e21e4
Mohan Krishnan <mohangk@gmail.com>
2012-01-31 16:43:36 +0800

    minor refactoring

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,14 +1,13 @@
 class Setting < ActiveRecord::Base
   validates :key, presence: true, uniqueness: true
   def self.key(key, value=nil)
+    setting = self.find_by_key(key)
 
     if value.nil?
-      setting = self.find_by_key(key)
       if not setting.nil?
         setting.value
       end
     else
-      setting = self.find_by_key(key)
       if setting.nil?
         setting = Setting.new
         setting.key = key
