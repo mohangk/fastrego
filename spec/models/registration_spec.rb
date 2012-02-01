a20e1add772f036ac78c8b246fdd48dd0eadcf77
Mohan Krishnan <mohangk@gmail.com>
2012-02-01 14:17:38 +0800

    added missing lines from registration spec back

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Registration do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :user }
+  it { should validate_uniqueness_of :user_id }
+  it { should validate_presence_of :user_id}
+  it { should validate_numericality_of(:debate_teams_requested)}
+  it { should validate_numericality_of(:adjudicators_requested)}
+  it { should validate_numericality_of(:observers_requested)}
+  it { should validate_numericality_of(:debate_teams_granted)}
+  it { should validate_numericality_of(:adjudicators_granted)}
+  it { should validate_numericality_of(:observers_granted)}
+  it { should validate_numericality_of(:debate_teams_confirmed)}
+  it { should validate_numericality_of(:adjudicators_confirmed)}
+  it { should validate_numericality_of(:observers_confirmed)}
+  it { should_not allow_mass_assignment_of(:debate_teams_granted)}
+  it { should_not allow_mass_assignment_of:adjudicators_granted)}
+  it { should_not allow_mass_assignment_of(:observers_granted)}
+  it { should_not allow_mass_assignment_of(:debate_teams_confirmed)}
+  it { should_not allow_mass_assignment_of(:adjudicators_confirmed)}
+  it { should_not allow_mass_assignment_of(:observers_confirmed)}
 end
