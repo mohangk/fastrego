05324389d617f621da4e0e413f1c79a9abbba151
Mohan Krishnan <mohangk@gmail.com>
2012-02-02 08:00:08 +0800

    institution now displays 'Add team manager' link that points to the sign up page

--- spec/requests/institutions_spec.rb
+++ spec/requests/institutions_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 
 describe "Institutions" do
   describe "list" do
-    it "provides a list of institutions with a link to create a team manager or display the name of one if already exists", do
+    it "provides a list of institutions with a link to create a team manager  or ", do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      dum = FactoryGirl.create(:institution, name: 'Dummy institution', abbreviation: 'DUM')
       mmu = FactoryGirl.create(:institution)
       visit institutions_path
       page.should have_content('Multimedia University')
@@ -12,14 +13,15 @@ describe "Institutions" do
       page.should have_content('http://www.mmu.edu.my')
       page.should have_css("a[href='/users/sign_up?institution_id=#{mmu.id}']")
       page.should have_link 'Click here to add your institution now'
+      click_link "add_team_manager_institution_#{mmu.id}"
+      page.should have_css("option[value='#{mmu.id}'][selected='selected']")
+    end
 
-      user = Factory(:user, institution: mmu)
-      user.institution = mmu
+    it "displays the name of a team manager if already exists" do
+      user = Factory(:user)
       visit institutions_path
-
-      page.should_not have_css("a[href='/users/sign_up?institution_id=#{mmu.id}']")
+      page.should_not have_css("a[href='/users/sign_up?institution_id=#{user.institution.id}']")
       page.should have_content('Suthen Thomas')
-
     end
   end
 
