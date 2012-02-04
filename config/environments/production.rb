b603612ef0164ae28a4af5426c884934d30b3328
Mohan Krishnan <mohangk@gmail.com>
2012-02-04 10:43:57 +0800

    config tidy up again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,9 +62,9 @@ UadcRego::Application.configure do
   config.action_mailer.default_url_options = {:host => 'uadc2012.heroku.com'}
   config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
-      :port => 25,
-      :address => ENV['MAILGUN_SMTP_PORT'],
-      :user_name => ENV['MAILGUN_SMTP_SERVER'],
+      :port => ENV['MAILGUN_SMTP_PORT'],
+      :address =>  ENV['MAILGUN_SMTP_SERVER'],
+      :user_name => ENV['MAILGUN_SMTP_LOGIN'],
       :password => ENV['MAILGUN_SMTP_PASSWORD'],
       :domain => 'uadc2012.herokuapp.com',
       :authentication => :plain,
