1f026da90f500d07c5810ff8736a33260ac85938
Mohan Krishnan <mohangk@gmail.com>
2012-02-09 07:56:01 +0800

    changed behaviour to display the registration details even if the enable_pre_registration is set to 'False'

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -43,7 +43,49 @@ describe "users/show.html.haml" do
         end
       end
 
-      context('after the team manager has submitted his registration') do
+      context('after the team manager has submitted his registration ') do
+        let(:user) do
+          user = Factory(:user)
+          user.confirm!
+          registration = Factory(:registration, user: user)
+          user.registration = registration
+          user
+        end
+
+        it 'will not provide the registration form' do
+          rendered.should_not have_css('form#new_registration')
+        end
+
+        it 'will display the datetime that the registration was requested' do
+          rendered.should have_content('You completed pre-registration at 2011-01-01 01:01:01 UTC and requested the following slots')
+        end
+
+        it 'will display the registration details' do
+          rendered.should have_content('3 debate teams')
+          rendered.should have_content('1 adjudicator')
+          rendered.should have_content('1 observer')
+        end
+      end
+    end
+
+    context('when when the system setting "enable_pre_registration" is disabled') do
+      before(:each) do
+        Setting.stub!(:key).and_return('False')
+        render
+      end
+      context('before the team manager has submitted his registration') do
+        it "will tell the team manager that the registration is open" do
+          rendered.should have_content('Registration is currently closed')
+          rendered.should_not have_content('Registration is open')
+        end
+        it "will hide the 3 text boxes to fill in the team managers  requests" do
+          rendered.should_not have_css('input#registration_debate_teams_requested')
+          rendered.should_not have_css('input#registration_adjudicators_requested')
+          rendered.should_not have_css('input#registration_observers_requested')
+        end
+      end
+
+      context('after the team manager has submitted his registration ') do
         let(:user) do
           user = Factory(:user)
           user.confirm!
