eaeaecfe294dfcf73b87b9bff4ce9d67a3f09ed9
Mohan Krishnan <mohangk@gmail.com>
2012-02-22 23:09:07 +0800

    modifies csv export of registration to match index page

--- app/admin/registrations.rb
+++ app/admin/registrations.rb
@@ -22,6 +22,25 @@ ActiveAdmin.register Registration do
     default_actions
   end
 
+  csv do
+    column('Req at') do |r|
+      r.requested_at.strftime("%d/%m %H:%M:%S")
+    end
+    column('Inst') do |r|
+      r.user.institution.abbreviation
+    end
+    column('DT Rq') { |r| r.debate_teams_requested }
+    column('Adj Rq') { |r| r.adjudicators_requested }
+    column('Obs Rq') { |r| r.observers_requested }
+    column('DT Gt') { |r| r.debate_teams_granted }
+    column('Adj Gt') { |r| r.adjudicators_granted }
+    column('Obs Gt') { |r| r.observers_granted }
+    column('DT Cnf') { |r| r.debate_teams_confirmed }
+    column('Adj Cnf') { |r| r.adjudicators_confirmed }
+    column('Obs Cnf') { |r| r.observers_confirmed }
+    column :fees
+  end
+
   form do |f|
     f.inputs do
       f.input :user, :input_html => { :disabled => true }
