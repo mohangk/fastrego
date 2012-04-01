4ee97c61e4c840a568303f128d6e45e315b231de
Mohan Krishnan <mohangk@gmail.com>
2012-04-01 23:49:46 +0800

    added seed.rb to help with new setups, updates TODO.txt for bipeds tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Setting.create(
+	[
+		{key: 'enable_pre_registration', value: 'False' },
+		{key: 'observer_fees', value: '100'},
+		{key: 'adjudicator_fees', value: '100'},
+		{key: 'debate_team_fees', value: '200'},
+		{key: 'debate_team_size', value: '3'}
+	])
