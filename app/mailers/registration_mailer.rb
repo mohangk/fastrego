7230e367c4f1d2a180d3fe50d2485fab9bcc6926
Mohan Krishnan <mohangk@gmail.com>
2012-04-13 00:28:51 +0800

    hooked notifications into registration model

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -2,19 +2,20 @@ class RegistrationMailer < ActionMailer::Base
   default from: ENV['MAILGUN_SMTP_LOGIN']
 
   def slots_granted_notification(registration)
-    self.slots_notification(registration, 'granted')
-  end
-
-  def slots_notification(registration, notification_type)
     @registration = registration 
-    @notification_type = notification_type
+    @notification_type = 'granted' 
     mail :to => registration.user.email, :subject => "#{@notification_type.capitalize} slots notification"  do |format|
       format.text { render 'slots_notification' }
       format.html { render 'slots_notification' }
     end
   end
-
+  
   def slots_confirmed_notification(registration)
-    self.slots_notification(registration, 'confirmed')
+    @registration = registration 
+    @notification_type = 'confirmed' 
+    mail :to => registration.user.email, :subject => "#{@notification_type.capitalize} slots notification"  do |format|
+      format.text { render 'slots_notification' }
+      format.html { render 'slots_notification' }
+    end
   end
 end
