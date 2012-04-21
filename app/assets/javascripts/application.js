fb99deb263086924ba87260a66ca14b462bc6652
Mohan Krishnan <mohangk@gmail.com>
2012-04-21 08:36:21 +0800

    wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require_directory .
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
