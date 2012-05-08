84e678aa4e380a304a0b6d5f78c0d81b145976fc
Mohan Krishnan <mohangk@gmail.com>
2012-05-09 06:52:26 +0800

    switched from using the HTML% js version to using iframe, js version does not seem to work with firefox 11

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require_directory .
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+
