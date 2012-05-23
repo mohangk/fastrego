e0b52e763ec5775b50346e2fff29d8633873b68f
Mohan Krishnan <mohangk@gmail.com>
2012-05-24 00:43:58 +0800

    added the active_admin assets to the precompile list

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module UadcRego
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    config.assets.precompile  += ['active_admin.css', 'active_admin.js']
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
