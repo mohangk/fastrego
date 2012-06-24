c9f21aba15978c50a98abcb23ccb0e88cd4e127c
Mohan Krishnan <mohangk@gmail.com>
2012-06-24 20:00:56 +0800

    Preliminary work on hstore_props implementation

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,29 +1,31 @@
 class Participant < ActiveRecord::Base
+  CUSTOM_FIELDS = %w(tshirt_size debate_experience)
   belongs_to :registration
 	validates_presence_of :name, :gender, :email, :dietary_requirement, :emergency_contact_person, :emergency_contact_number, :registration
 
+  #TODO: Figure out why after_initialize is not being called when Participant is a nested attribute
   after_initialize :setup_hstore
   
   def setup_hstore
     self.data ||= {} 
   end
-  
-  def tshirt_size 
-    self.data['tshirt_size'].nil? ? nil :  self.data['tshirt_size'].to_i
+
+  def self.custom_fields
+    CUSTOM_FIELDS
   end
 
-  def tshirt_size=(tshirt_size)
-    self.send(:attribute_will_change!, 'data')
-    self.data['tshirt_size'] = tshirt_size
-  end
- 
-  def debate_experience
-    self.data['debate_experience'].nil? ? nil :  self.data['debate_experience'].to_i
-  end
+  CUSTOM_FIELDS.each do |attr_name|
 
-  def debate_experience=(debate_experience)
-    self.send(:attribute_will_change!,'data')
-    self.data['debate_experience'] = debate_experience
-  end
+    define_method "#{attr_name}=" do |attr_value|
+      setup_hstore
+      self.send(:attribute_will_change!, 'data')
+      self.data[attr_name] = attr_value
+    end
 
- end
+    define_method attr_name do 
+      setup_hstore
+      self.data[attr_name].nil? ? nil :  self.data[attr_name]
+    end
+
+  end  
+end
--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -10,6 +10,31 @@ describe Participant do
   it { should validate_presence_of :emergency_contact_person }
   it { should validate_presence_of :emergency_contact_number }
   it { should have_db_column(:data).of_type(:hstore) }
+  
+  describe 'initialize data attr' do
+    it 'sets it to a hash' do
+      Participant.new.data.should == {}
+    end
+
+    context 'when used as a nested attribute' do
+
+      it 'set it to a hash' do
+        test_attributes = {
+          "adjudicators_attributes" =>
+             {"0"=>{"name"=>"Mohan",
+                    "gender"=>"Male",
+                    "email"=>"test@email.com",
+                    "emergency_contact_person"=>"test",
+                    "emergency_contact_number"=>"123123",
+                    "preferred_roommate"=>"",
+                    "preferred_roommate_institution"=>"",
+                    "tshirt_size"=>"tshirt large",
+                    "debate_experience"=>"lots of experience"}}} 
+        r = FactoryGirl.create(:registration)
+        r.update_attributes(test_attributes)
+      end
+    end
+  end
 
   describe 'custom fields' do
     let(:observer) do 
@@ -20,7 +45,7 @@ describe Participant do
       observer.debate_experience.should == nil
     end
 
-    it 'reconginizes changes to the custom fields' do
+    it 'recognizes changes to the custom fields' do
       observer.debate_experience = 5
       observer.data_changed?.should == true
       observer.save!
@@ -28,9 +53,16 @@ describe Participant do
       observer.tshirt_size = 'new field test' 
       observer.data_changed?.should == true 
     end
+
+    it 'persists the custom fields' do
+      observer.debate_experience = 'lots of experience'
+      observer.save!
+      observer_id = observer.id
+      o = Observer.find(observer_id)
+      o.debate_experience.should == 'lots of experience'
+    end
   end
 end
-
 
 
 
