267a4bbc6bdb6785ba2e653a5174ce653cd71f6e
Mohan Krishnan <mohangk@gmail.com>
2012-09-01 15:50:01 +0800

    Removes ability to create,edit and delete team_managers from admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,20 +1,14 @@
 ActiveAdmin.register User do
-  actions :all, :except => [:new, :edit, :delete]
+  actions :all, :except => [:new, :edit, :destroy]
   scope_to association_method: :call do
     lambda { User.where(id: current_admin_user.registrations.collect { |r| r.team_manager.id }) }
   end
   menu label: 'Team manager'
   
-  #scope :all, :default => true do |users|
-  #  users.includes [:registraton]
-  #end
-
-
   index do
     column :id
     column :name
     column :email
-    #column :institution, :sortable => :'institutions.name'
     column :phone_number
     column :last_sign_in_at
     column :last_sign_in_ip
@@ -23,42 +17,18 @@ ActiveAdmin.register User do
     default_actions
   end
 
-  #filter :institution, collection: proc { Institution.order(:name).all }
   filter :email
   filter :name
   filter :phone_number
 
-  #TODO: not putting the collection value into a proc might result in the "table not found" issue
   form do |f|
     f.inputs "Team manager details" do
-      #f.input :institution, collection:  Institution.order(:name).all 
       f.input :name
       f.input :email
       f.input :password
       f.input :password_confirmation
       f.input :phone_number
-      f.input :send_reset_password_email, as: :boolean, default: true,
-              label: 'Select this checkbox if you would like to send an email to the team manager to reset her account password'
+      f.buttons
     end
-    f.buttons
   end
-
-  controller do
-    def create
-      @user = User.new(params[:user])
-      #if the admin creates the user, we skip confirmation and rely 
-      @user.skip_confirmation!
-      @user.send_reset_password_instructions if params[:user][:send_reset_password_email] == '1'
-
-      respond_to do |format|
-        if @user.save
-          format.html { redirect_to admin_users_url, notice: 'Team manager was successfully created.' }
-        else
-          format.html { render action: "new" }
-        end
-      end
-    end
-
-  end
-
 end
--- spec/requests/admin_team_managers.rb
+++ spec/requests/admin_team_managers.rb
@@ -6,32 +6,27 @@ describe 'AdminInstitution' do
 
   describe 'create and edit team manager' do
 
-    let!(:institution) { FactoryGirl.create :institution }
+    let!(:t2) { FactoryGirl.create(:t2_tournament) }
+    let!(:t1) { FactoryGirl.create(:t1_tournament) }
+    let!(:t1_team_manager) { FactoryGirl.create(:user, email: 't1_team_manager@test.com') }
+    let!(:t2_team_manager) { FactoryGirl.create(:user, email: 't2_team_manager@test.com') }
+    let!(:t1_registration) { FactoryGirl.create(:registration, tournament: t1, team_manager: t1_team_manager) }
+    let!(:t2_registration) { FactoryGirl.create(:registration, tournament: t2, team_manager: t2_team_manager) }
 
-    it 'allows for a team manager to be created and edited' do
-      t1 = FactoryGirl.create(:t1_tournament)
-      set_subdomain 't1'
-      login t1.admin_user.email, t1.admin_user.password
+    it 'only list team managers who have registered for this tournament to be only viewed, not edit, deleted or created' do
+      login_for_tournament(t1)
       visit admin_users_path
       page.should have_content 'Team manager'
-      click_link 'New User'
-      select institution.name, from: 'Institution'
-      fill_in 'Name',  with: 'Test team manager'
-      fill_in 'Email', with: 'test@test.com'
-      fill_in 'Password', with: 'password'
-      fill_in 'Password confirmation', with: 'password' 
-      fill_in 'Phone number', with: '123123123'
-      click_button 'Create User'
+
       visit admin_users_path
-      page.should have_content 'Test team manager'
-      click_link 'Edit'
-      page.has_field? 'Name', with: 'Test team manager' 
-      fill_in 'Name', with: 'Test team 2 manager'
-      fill_in 'Password', with: 'password'
-      fill_in 'Password confirmation', with: 'password' 
-      click_button 'Update User'
-      visit admin_users_path
-      page.should have_content 'Test team 2 manager'
+      page.should_not have_content t2_team_manager.email
+      page.should have_content t1_team_manager.email
+
+      page.should_not have_content 'New'
+      page.should_not have_content 'Edit'
+      page.should_not have_content 'Delete'
+      click_link 'View'
+      page.should have_content t1_team_manager.name
     end
   end
 end
