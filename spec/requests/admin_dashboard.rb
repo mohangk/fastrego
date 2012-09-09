e3fd6b2b1d73e3f1279e949389f049f3a11ea95e
Mohan Krishnan <mohangk@gmail.com>
2012-09-09 20:00:45 +0800

    Adds place holder spec for admin dashboard

---
+++ spec/requests/admin_dashboard.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+require_relative './admin_helpers.rb'
+
+describe 'AdminLogin' do
+  include AdminHelpers
+  describe 'dashboard numbers are scoped to the current tournament' do 
+
+    pending 'it should only show you t1 tournament figures'
+  end
+end
+
