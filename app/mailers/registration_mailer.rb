db9bc2a93a9e7d5787422753456177c599515576
Mohan Krishnan <mohangk@gmail.com>
2012-09-22 11:21:13 +0800

    Minor refactoring and logger to debug

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,16 +3,23 @@ class RegistrationMailer < ActionMailer::Base
   def slots_granted_notification(registration)
     @registration = registration 
     @notification_type = 'granted' 
-    mail from: Setting.key(registration.tournament, 'tournament_registration_email'),:to => registration.team_manager.email, :subject => "[#{registration.tournament.identifier}] Updates to your #{@notification_type} slots!"  do |format|
-      format.text { render 'slots_notification' }
-      format.html { render 'slots_notification' }
-    end
+    notification(@registration, @notification_type) 
   end
   
   def slots_confirmed_notification(registration)
     @registration = registration 
     @notification_type = 'confirmed' 
-    mail :to => registration.team_manager.email, :subject => "[#{registration.tournament.identifier}] Updates to your #{@notification_type} slots!"  do |format|
+    notification(@registration, @notification_type) 
+  end
+
+  private
+
+  def notification(registration, notification_type)
+    from = Setting.key(registration.tournament, 'tournament_registration_email')
+    subject = "[#{registration.tournament.identifier}] Updates to your #{@notification_type} slots!"
+    to = registration.team_manager.email
+    logger.error "REGISTRATION LOG => From: #{from}, To: #{to}, Subject: #{subject}"
+    mail from: from, to: to, subject: subject do |format|
       format.text { render 'slots_notification' }
       format.html { render 'slots_notification' }
     end
