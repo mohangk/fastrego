c659fd638c4be81fa7e06c854eb5742f13a1378d
Mohan Krishnan <mohangk@gmail.com>
2012-10-02 06:33:33 +0800

    Fixes signup link

--- spec/requests/login_spec.rb
+++ spec/requests/login_spec.rb
@@ -1,5 +1,29 @@
 describe "Login" do
 
-  it 'raises a 404 when trying to access a subdomain that does not exist'
-  it 'shares logins between tournaments'
+  let!(:t1) { FactoryGirl.create(:t1_tournament) } 
+
+  before(:each) do
+    set_subdomain t1.identifier
+  end
+
+  it 'has a sign up form' do
+    visit profile_path
+    click_link 'Sign up'
+    page.should have_content 'Sign up'
+  end
+
+  it 'has a recover password form' do
+    visit profile_path
+    click_link 'Forgot your password?'
+    page.should have_content 'Forgot your password?'
+  end
+
+  it 'has a recover password form' do
+    visit profile_path
+    click_link "Didn't receive confirmation instructions?"
+    page.should have_content "Resend confirmation instructions"
+  end
+ 
+  pending 'it raises a 404 when tryign to access a subdomain that does not exist'
+  pending 'shares logins between tournaments'
 end
