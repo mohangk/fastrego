3b80a1b53c3af2e12dfb7fa091fc735f0d23f8c1
Mohan Krishnan <mohangk@gmail.com>
2013-01-11 01:24:13 +0800

    Updates the frontpage tournament list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   layout 'pages_layout'
-  
+
   def embed_logo
   end
 
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
     @enquiry = Enquiry.new
   end
 
-  def send_enquiry 
+  def send_enquiry
     @enquiry = Enquiry.new(params[:enquiry])
     if @enquiry.valid?
       EnquiryMailer.send_enquiry(@enquiry).deliver
@@ -21,24 +21,27 @@ class PagesController < ApplicationController
 
   def homepage
 
-    @current_tournaments = { 
-      'Berlin WUDC 2013' => 'http://wudcberlin.herokuapp.com'
+    @current_tournaments = {
+      'Canton IV 2013' => 'http://cantoniv2013.herokuapp.com',
+      'Malaysian Debate Open 2013' => 'http://mdo2013.herokuapp.com',
+      'World Debate Forum 2013' => 'http://wdf2013.herokuapp.com'
     }
 
     @past_tournaments = {
-      'North East Asians 2012' => 'http://neao2012.herokuapp.com',
-      'MMU UADC 2012'  => 'http://uadc2012.herokuapp.com',
-      'BIPEDS Asian BP 2012' => 'http://bipedsabp.herokuapp.com',
-      'Summer Asian Debate Institute 2012' => 'http://asiandebateinstitute.herokuapp.com',
-      '11th Korean National Championships' => 'http://solbridgeknc.herokuapp.com',
-      'Philippine School Debates 2012' => 'http://psdc2012.herokuapp.com' 
+      'Berlin WUDC 2013' => 'http://wudcberlin.herokuapp.com',
+      'North East Asians 2012' => nil,
+      'MMU UADC 2012'  => nil,
+      'BIPEDS Asian BP 2012' => nil, 
+      'Summer Asian Debate Institute 2012' => nil,
+      '11th Korean National Championships' => nil,
+      'Philippine School Debates 2012' => nil
     }
 
     Tournament.all.each do |t|
       if t.active == true
-        @current_tournaments[t.name] = t.url    
+        @current_tournaments[t.name] = t.url
       elsif t.active == false
-        @past_tournaments[t.name] = t.url    
+        @past_tournaments[t.name] = t.url
       end
     end
   end
