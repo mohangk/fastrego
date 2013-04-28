98dbf0564fcc20e81b3d172a2e47abe112b2eaa5
Mohan Krishnan <mohangk@gmail.com>
2013-04-28 21:28:59 +0800

    Refactor long link_to links into paypal_button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+  PAYPAL_EXPRESS_BUTTON = "https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif"
+
+  def paypal_button pre_registration = false
+
+    if pre_registration
+      title = 'Pay pre registration now via PayPal'
+      params = { type: 'pre_registration' }
+    else
+      title = 'Pay now via PayPal'
+      params = {}
+    end
+
+    link_to image_tag(PAYPAL_EXPRESS_BUTTON), checkout_path(params), title: title, method: :post
+  end
 end
